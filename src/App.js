af4b1219a962518068f623d2fe160293ed5cb9d0
DanielLaV <djjlav@gmail.com>
2021-12-28 15:36:58 -0600

    finished phase 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import GalleryNavigation from './components/GalleryNavigation';
 import harvardArt from './data/harvardArt';
 
 function App() {
   return (
     <div>
-      <h1>Hi Daniel</h1>
+      <GalleryNavigation galleries={harvardArt.records}/>
     </div>
   );
 }
